4ae1dd32e155431e240a4cb42ef7c39fac90b3d8
cmichi <mich@elmueller.net>
2013-06-05 23:36:14 +0200

    added correct svg processing

--- render.js
+++ render.js
@@ -248,5 +248,17 @@ function createFile() {
 	}
 
 	fs.writeFileSync("./processing/data.asc", paths_file.join('\n'), "utf8");
-	fs.writeFileSync("output.svg", paths.join('\n'), "utf8");
+
+	var svgContent = '<?xml version="1.0" encoding="UTF-8"?>'
+	+ '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" '
+	+ '"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"> '
+	+ '<svg xmlns="http://www.w3.org/2000/svg" '
+	+ 'xmlns:xlink="http://www.w3.org/1999/xlink" '
+	+ 'xmlns:ev="http://www.w3.org/2001/xml-events" '
+	+ 'version="1.1" baseProfile="full" '
+	+ 'width="800" height="800">\n'
+	+ paths.join('\n') 
+	+ "\n</svg>";
+
+	fs.writeFileSync("output.svg", svgContent, "utf8");
 }
